245d6e2cc5c561c2c6db35d71d2c9b31ed5b7cfa
bharathrajip <bharathrajibs@gmail.com>
2025-08-24 15:43:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function changeText() {
+  document.getElementById("heading").innerText = "Welcome to JavaScript!";
+}
+for (let i = 1; i <= 10; i++) {
+   if (i % 2 === 0) {
+      console.log(i);
+   }
+}
+
